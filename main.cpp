9d9058b44bd424e99c99f2478d89fe58c7eb185b
YoriichiHIDRA <m64450998@gmail.com>
2025-12-28 11:26:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,17 +9,13 @@ bool isL(char c) {
     if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
         return true;
     }
-
-
     return false;
 }
 
 char toL(char c) {
-
     if (c >= 'A' && c <= 'Z') {
         return c + 32;
     }
-
     return c;
 }
 
@@ -38,80 +34,81 @@ int main() {
 
     const int NUM_WORDS = sizeof(words) / sizeof(words[0]);
 
-    const char* word = words[rand() % NUM_WORDS];
-
-    string guessed;
-    int word_len = 0;
-
-    while (word[word_len] != '\0') {
-        guessed += '_';
-        word_len++;
-    }
-
-    int attempts = 6;
-    char letter;
-    bool win = false;
-
-    cout << "Добро пожаловать в игру: 'Виселица'!\n";
-    cout << "Угадайте слово из " << word_len << " букв.\n";
-    cout << "У вас есть " << attempts << " попыток.\n\n";
-
-    while (attempts > 0 && !win) {
-        cout << "Слово: ";
-        for (int i = 0; i < word_len; i++) {
-            cout << guessed[i] << " ";
+    while (true) {   
+        const char* word = words[rand() % NUM_WORDS];
+        
+        string guessed;
+        int word_len = strlen(word);
+        for(int i = 0; i < word_len; ++i) {
+            guessed += '_';
         }
 
-        cout << "\nОсталось попыток: " << attempts << endl;
-        cout << "Введите букву: ";
-        cin >> letter;
+        int attempts = 6;
+        bool win = false;
 
-        if (!isL(letter)) {
-            cout << "Ошибка! Введите букву.\n\n";
-            continue;
-        }
+        cout << "Добро пожаловать в игру: 'Виселица'!\n";
+        cout << "Угадайте слово из " << word_len << " букв.\n";
+        cout << "У вас есть " << attempts << " попыток.\n\n";
 
-        letter = toL(letter);
+        while(attempts > 0 && !win) {
 
-        bool found = false;
-
-        for (int i = 0; i < word_len; i++) {
-            char word_letter = toL(word[i]);
-
-            if (word_letter == letter && guessed[i] == '_') {
-                guessed[i] = word[i];
-                found = true;
-            }
-        }
-
-        if (found) {
-            cout << "Верно! Буква '" << letter << "' есть в слове.\n";
-
-            win = true;
+            cout << "Слово: ";
             for (int i = 0; i < word_len; i++) {
-                if (guessed[i] == '_') {
-                    win = false;
-                    break;
+                cout << guessed[i] << " ";
+            }
+            
+            cout << "\nОсталось попыток: " << attempts << endl;
+            cout << "Введите букву: ";
+            char letter;
+            cin >> letter;
+
+            if(!isL(letter)) {
+                cout << "Ошибка! Введите букву.\n\n";
+                continue;
+            }
+
+            letter = toL(letter);
+
+            bool found = false;
+
+            for(int i = 0; i < word_len; i++) {
+                char word_letter = toL(word[i]);
+                
+                if(word_letter == letter && guessed[i] == '_') {
+                    guessed[i] = word[i];
+                    found = true;
                 }
             }
+
+            if(found) {
+                cout << "Верно! Буква '" << letter << "' есть в слове.\n";
+
+                win = true;
+                for(int i = 0; i < word_len; i++) {
+                    if(guessed[i] == '_') {
+                        win = false;
+                        break;
+                    }
+                }
+            }
+            else {
+                attempts--;
+                cout << "Нет такой буквы! Осталось попыток: " << attempts << "\n";
+            }
+
+            cout << endl;
+        }
+
+        if(win) {
+            cout << "\n\t\tПоздравляем!\t\t\t\n";
+            cout << "Вы разумный и смышлённый такие годитесь к нам в Интерпол!\n";
+            cout << "Поздравляем! Вы угадали слово: " << word << endl;
         }
         else {
-            attempts--;
-            cout << "Нет такой буквы! Попыток осталось: " << attempts << "\n";
+            cout << "Деградация на высшем уровне!\n";
+            cout << "Вы проиграли! Загаданное слово было: " << word << endl;
         }
-
-        cout << endl;
     }
 
-    if (win) {
-        cout << "\n\t\tПоздравляем!\t\t\t\n";
-        cout << "Вы разумный и смышлённый такие годятся к нам в интерпол!\n";
-        cout << "Поздравляем! Вы угадали слово: " << word << endl;
-    }
-    else {
-        cout << "Деградация на вышем уровне!"<< endl;
-        cout << "Вы проиграли! Загаданное слово было: " << word << endl;
-    }
-return 0;
+    return 0;
 }
-
